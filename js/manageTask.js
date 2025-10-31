9bb188bd3fe6422f5a4c386da513dd192c60ed96
unknown <betranarya03@gmail.com>
2025-10-31 23:22:21 +0700

    tasks commit

---
+++ js/manageTask.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // membuat instance dari object task 
+    const myTasks = new Task();
+
+    // membuat variable untuk mengambil seluruh task 
+    const existingTasks = myTask.getTasks();
+
+});
